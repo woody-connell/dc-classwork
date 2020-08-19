26a3345cdaf3c6ee68c78c20e7105e7c5c077701
woody-connell <woodyconnell@gmail.com>
2020-08-19 13:20:37 -0500

    update

--- week12/3-Wednesday/preLectureNotes/lifecycle/src/App.js
+++ week12/3-Wednesday/preLectureNotes/lifecycle/src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
+import Loading from './Loading'
+import Child from './Child'
 
 class App extends Component {
+
+constructor(props) {
+  super(props)
+  console.log("constructor called")
+
+  this.state = {
+    isLoaded: false
+  }
+}
+
+componentDidMount = () => {
+  console.log('component did mount')
+
+  // api
+
+  setTimeout(() => {
+    this.setState(
+      {
+        isLoaded: true
+      }
+    )
+  }, 3000)
+}
+
+
+
   render() {
+    console.log('home page rendered')
+
+    if(!this.state.isLoaded) {
+      return <Loading />
+    }
+    else {
     return (
       <>
-        
+        Home Page
       </>
     )
+    }
   }
 }
 
---
+++ week12/3-Wednesday/preLectureNotes/lifecycle/src/Child.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Child = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Child
+
+
