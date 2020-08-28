8c7bad64844d08a8b366a545cce957bcc4e5fd51
Veronica <veronica@digitalcrafts.com>
2020-08-28 10:21:57 -0500

    redux to local storage

--- week13/3-Wednesday/preLectureNotes/redux-template/src/index.js
+++ week13/3-Wednesday/preLectureNotes/redux-template/src/index.js
@@ -8,12 +8,51 @@ import reducer from './reducers/reducer'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import BaseLayout from './components/layout/BaseLayout'
 
-let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())// creating store inside of redux
+
+let saveToLocalStorage = (state) =>{
+
+  try{
+    const serializeState  = JSON.stringify(state);
+
+    localStorage.setItem('state', serializeState);
+
+    //localStorage.state = serializeState;
+  }
+  catch(e){
+    console(e)
+  }
+
+}
+
+let loadFromLocalStorage = () =>{
+
+  const serializeState = localStorage.getItem('state');
+  if (serializeState == null){
+    return undefined;
+  }
+  else {
+    return JSON.parse(serializeState);
+  }
+
+}
+
+const persistedState = loadFromLocalStorage();
+
+
+let store = createStore(reducer, persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())// creating store inside of redux
+
+store.subscribe(()=>{
+
+  //call a function pass the state
+
+  //state.getState()
+  saveToLocalStorage(store.getState());
+})
 
 ReactDOM.render(
   
   <Provider store={store}>
-
     <BrowserRouter>
     <BaseLayout>
       <Switch>
